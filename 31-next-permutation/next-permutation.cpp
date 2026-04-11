fb55f416920864bb192991565e9ef38a742df274
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-04-11 17:59:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.05%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: Find first decreasing element from right
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If found, find just larger element on right
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
