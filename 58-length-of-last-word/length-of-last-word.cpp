60e73a734826a4b8036e402e122ba150ebca1c53
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-04-18 09:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (32.94%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        int i = s.size() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        // Count length of last word
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
