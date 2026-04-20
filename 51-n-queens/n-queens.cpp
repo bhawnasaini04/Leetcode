3f579991470be00b89d016aab4706a14f6a61f90
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-04-20 20:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (93.04%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> result;
+        vector<string> board(n, string(n, '.'));
+        
+        backtrack(0, n, board, result);
+        return result;
+    }
+    
+    bool isSafe(int row, int col, vector<string>& board, int n) {
+        // check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+        
+        // check upper-left diagonal
+        for (int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        
+        // check upper-right diagonal
+        for (int i = row-1, j = col+1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        
+        return true;
+    }
+    
+    void backtrack(int row, int n, vector<string>& board, vector<vector<string>>& result) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+        
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';     // place queen
+                backtrack(row + 1, n, board, result);
+                board[row][col] = '.';     // backtrack
+            }
+        }
+    }
+};
