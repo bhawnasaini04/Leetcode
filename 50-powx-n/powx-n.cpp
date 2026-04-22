6d8784b87c258ea07d04e36a3b058f7dae2f5020
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-04-22 20:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (43.92%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n; // handle INT_MIN case
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1.0;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        
+        return result;
+    }
+};
