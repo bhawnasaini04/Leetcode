fc54b5f5300cab4c78d6c32b4da6c20fe81be449
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-01 08:59:51 +0530

    Time: 5 ms (17.40%) | Memory: 9.1 MB (50.65%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = s.size(), m = p.size();
-
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-
+        int m = s.size(), n = p.size();
+        
+        // dp[i][j] = whether s[0..i-1] matches p[0..j-1]
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
         dp[0][0] = true;
-
+        
         // Handle patterns like a*, a*b*, etc.
-        for (int j = 2; j <= m; j++) {
+        for (int j = 2; j <= n; j++) {
             if (p[j - 1] == '*') {
                 dp[0][j] = dp[0][j - 2];
             }
         }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-
-                // Case 1: direct match or '.'
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                
                 if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
                     dp[i][j] = dp[i - 1][j - 1];
                 }
-
-                // Case 2: '*'
+                
                 else if (p[j - 1] == '*') {
-                    // zero occurrence
+                    // Case 1: zero occurrence
                     dp[i][j] = dp[i][j - 2];
-
-                    // one or more occurrence
+                    
+                    // Case 2: one or more occurrence
                     if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
                         dp[i][j] = dp[i][j] || dp[i - 1][j];
                     }
                 }
             }
         }
-
-        return dp[n][m];
+        
+        return dp[m][n];
     }
 };
