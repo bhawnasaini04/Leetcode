2b5f9ea8fa4497cc95f76c25727804199a19b2a2
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-02 17:39:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.19%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+private:
+    bool isGood(int num) {
+        bool changed = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            // Invalid digits
+            if (digit == 3 || digit == 4 || digit == 7) {
+                return false;
+            }
+
+            // Digits that change after rotation
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                changed = true;
+            }
+
+            num /= 10;
+        }
+
+        return changed; // must change to be "good"
+    }
+};
