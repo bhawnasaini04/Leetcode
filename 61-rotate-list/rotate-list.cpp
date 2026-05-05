b0d9ae80f63086bebe671b3537ba183c7174cea5
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-05 11:53:28 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (31.20%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -3,29 +3,29 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if (!head || !head->next || k == 0) return head;
 
-        // Step 1: Find length and tail
+        // Step 1: Find length and last node
         ListNode* temp = head;
-        int n = 1;
+        int len = 1;
         while (temp->next) {
             temp = temp->next;
-            n++;
+            len++;
         }
 
         // Step 2: Make it circular
         temp->next = head;
 
         // Step 3: Find new tail
-        k = k % n;
-        int stepsToNewTail = n - k;
-        ListNode* newTail = head;
+        k = k % len;
+        int stepsToNewTail = len - k;
 
+        ListNode* newTail = head;
         for (int i = 1; i < stepsToNewTail; i++) {
             newTail = newTail->next;
         }
 
-        // Step 4: Set new head and break the cycle
+        // Step 4: Break the circle
         ListNode* newHead = newTail->next;
-        newTail->next = nullptr;
+        newTail->next = NULL;
 
         return newHead;
     }
