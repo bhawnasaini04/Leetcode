0d45df555a66870da3959fcf1b76f0b5d876d94f
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-07 11:46:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (58.61%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+
+            // If today's price is greater than yesterday's
+            if (prices[i] > prices[i - 1]) {
+
+                // Add the profit
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+};
