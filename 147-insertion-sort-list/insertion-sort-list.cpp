a7b9a70fe3601084ea25dd5daa0b3f59572e370e
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-09 21:35:52 +0530

    Time: 27 ms (23.31%) | Memory: 14.5 MB (96.86%) - LeetSync

---
+++ 147-insertion-sort-list/insertion-sort-list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode dummy(0);
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* prev = &dummy;
+
+            while (prev->next && prev->next->val < curr->val) {
+                prev = prev->next;
+            }
+
+            ListNode* nextNode = curr->next;
+
+            curr->next = prev->next;
+            prev->next = curr;
+
+            curr = nextNode;
+        }
+
+        return dummy.next;
+    }
+};
