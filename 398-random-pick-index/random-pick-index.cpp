a278a43a932234c9626ea48dc829c2b8a8fcb268
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-28 19:13:23 +0530

    Time: 1688 ms (5.18%) | Memory: 50.5 MB (75.46%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nums;
+
+    Solution(vector<int>& nums) {
+        this->nums = nums;
+    }
+
+    int pick(int target) {
+
+        int result = -1;
+        int count = 0;
+
+        for(int i=0;i<nums.size();i++) {
+
+            if(nums[i] == target) {
+
+                count++;
+
+                if(rand() % count == 0)
+                    result = i;
+            }
+        }
+
+        return result;
+    }
+};
