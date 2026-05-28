7dcd14ca71866eda15686281a66861fee068615a
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-28 19:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (97.00%) - LeetSync

---
+++ 393-utf-8-validation/utf-8-validation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+
+        int remaining = 0;
+
+        for(int num : data) {
+
+            if(remaining == 0) {
+
+                if((num >> 5) == 0b110)
+                    remaining = 1;
+
+                else if((num >> 4) == 0b1110)
+                    remaining = 2;
+
+                else if((num >> 3) == 0b11110)
+                    remaining = 3;
+
+                else if((num >> 7))
+                    return false;
+            }
+            else {
+
+                if((num >> 6) != 0b10)
+                    return false;
+
+                remaining--;
+            }
+        }
+
+        return remaining == 0;
+    }
+};
