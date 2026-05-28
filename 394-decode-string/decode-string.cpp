3ab8a00f6085a11cfa9a1e44a86e8a2d1c6e38de
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-28 19:12:24 +0530

    Time: 1 ms (27.64%) | Memory: 9.5 MB (34.00%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string decodeString(string s) {
+
+        stack<int> countStack;
+        stack<string> strStack;
+
+        string curr = "";
+        int num = 0;
+
+        for(char ch : s) {
+
+            if(isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if(ch == '[') {
+
+                countStack.push(num);
+                strStack.push(curr);
+
+                num = 0;
+                curr = "";
+            }
+
+            else if(ch == ']') {
+
+                int repeat = countStack.top();
+                countStack.pop();
+
+                string temp = strStack.top();
+                strStack.pop();
+
+                while(repeat--)
+                    temp += curr;
+
+                curr = temp;
+            }
+
+            else {
+                curr += ch;
+            }
+        }
+
+        return curr;
+    }
+};
