143f9275e3fed1dfa890a5ec6fef277251e332ac
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-05-31 14:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (81.82%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSegments(string s) {
+
+        int count = 0;
+
+        for(int i=0;i<s.size();i++) {
+
+            if((i==0 || s[i-1]==' ')
+               && s[i]!=' ')
+                count++;
+        }
+
+        return count;
+    }
+};
