667e1054f389f08574546db9838c6f410e1915f7
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-06-02 20:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (61.38%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+
+        unordered_map<char,int> freq;
+
+        for (char c : s)
+            freq[c]++;
+
+        priority_queue<
+            pair<int,char>
+        > pq;
+
+        for (auto& p : freq)
+            pq.push({p.second, p.first});
+
+        string result;
+
+        while (!pq.empty()) {
+
+            auto [count, ch] = pq.top();
+            pq.pop();
+
+            result.append(count, ch);
+        }
+
+        return result;
+    }
+};
