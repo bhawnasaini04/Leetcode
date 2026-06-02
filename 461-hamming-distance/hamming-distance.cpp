f4a0a423df42efcddeda2de9848d078d1d9710f0
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-06-02 20:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.02%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+
+        int diff = x ^ y;
+        int count = 0;
+
+        while (diff) {
+            count += diff & 1;
+            diff >>= 1;
+        }
+
+        return count;
+    }
+};
