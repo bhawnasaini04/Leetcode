bcaed8ffde783712f0008512aab940c3e47dc4d7
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-06-21 19:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (15.17%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+
+        int absent = 0;
+        int late = 0;
+
+        for (char c : s) {
+
+            if (c == 'A') {
+                absent++;
+                late = 0;
+
+                if (absent >= 2)
+                    return false;
+            }
+            else if (c == 'L') {
+
+                late++;
+
+                if (late >= 3)
+                    return false;
+            }
+            else {
+                late = 0;
+            }
+        }
+
+        return true;
+    }
+};
