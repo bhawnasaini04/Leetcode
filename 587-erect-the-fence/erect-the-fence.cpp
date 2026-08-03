ea49583548e1cd2a8c24c31c481579e93deea052
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-08-03 18:47:31 +0530

    Time: 14 ms (66.85%) | Memory: 27.1 MB (66.48%) - LeetSync

---
+++ 587-erect-the-fence/erect-the-fence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int cross(vector<int>& O, vector<int>& A, vector<int>& B) {
+        return (A[0] - O[0]) * (B[1] - O[1]) -
+               (A[1] - O[1]) * (B[0] - O[0]);
+    }
+
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+
+        int n = trees.size();
+        if (n <= 3)
+            return trees;
+
+        sort(trees.begin(), trees.end());
+
+        vector<vector<int>> hull;
+
+        // Lower Hull
+        for (auto &p : trees) {
+            while (hull.size() >= 2 &&
+                   cross(hull[hull.size()-2], hull[hull.size()-1], p) < 0)
+                hull.pop_back();
+
+            hull.push_back(p);
+        }
+
+        int t = hull.size() + 1;
+
+        // Upper Hull
+        for (int i = n - 2; i >= 0; i--) {
+            while (hull.size() >= t &&
+                   cross(hull[hull.size()-2], hull[hull.size()-1], trees[i]) < 0)
+                hull.pop_back();
+
+            hull.push_back(trees[i]);
+        }
+
+        hull.pop_back();
+
+        sort(hull.begin(), hull.end());
+        hull.erase(unique(hull.begin(), hull.end()), hull.end());
+
+        return hull;
+    }
+};
