67e47b26e81cd8436654fdff721e86250e4fa70c
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-08-10 18:54:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (39.16%) - LeetSync

---
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findIntegers(int n) {
+
+        vector<int> dp(32, 0);
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < 32; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int ans = 0;
+        int prevBit = 0;
+
+        for (int i = 30; i >= 0; i--) {
+
+            if (n & (1 << i)) {
+
+                ans += dp[i];
+
+                // Found consecutive 1s
+                if (prevBit == 1) {
+                    return ans;
+                }
+
+                prevBit = 1;
+            }
+            else {
+                prevBit = 0;
+            }
+        }
+
+        // Include n itself
+        return ans + 1;
+    }
+};
