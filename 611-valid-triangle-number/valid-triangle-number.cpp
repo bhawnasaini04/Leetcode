337e987dcf45a892367ee4bd9a8e27a5178bbf67
Bhawna Saini <bhawnasaini9138@gmail.com>
2026-08-11 17:46:23 +0530

    Time: 35 ms (36.24%) | Memory: 16.5 MB (78.95%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        int count = 0;
+
+        // k is the largest side
+        for (int k = n - 1; k >= 2; k--) {
+
+            int i = 0;
+            int j = k - 1;
+
+            while (i < j) {
+
+                if (nums[i] + nums[j] > nums[k]) {
+
+                    // Every number from i to j-1
+                    // can form a triangle with j and k
+                    count += j - i;
+
+                    j--;
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
